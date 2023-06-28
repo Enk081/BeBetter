53904eb28e0d32610fba621a69d5d4096b319e1a
Enk081 <55228129+Enk081@users.noreply.github.com>
2023-06-28 20:58:55 +0800

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+		function hli() {
+			less1.style.width='60%';
+			happy.innerHTML='HAPPINESS';
+			sad.innerHTML='ADNESS';
+		}
+
+
+		function hlo() {
+			less1.style.width='50%';
+			happy.innerHTML='HAPPINES';
+			sad.innerHTML='SADNESS';
+		}
+		
+		function sli() {
+			less1.style.width='40%';
+			happy.innerHTML='HAPPINES';
+			sad.innerHTML='SADNESS';
+		}
+
+
+		function slo() {
+			less1.style.width='50%';
+			happy.innerHTML='HAPPINES';
+			sad.innerHTML='SADNESS';
+		}
